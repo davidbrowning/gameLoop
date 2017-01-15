62c8a9a7dddc907d29a3de1c9fa55202bcd90635
dmbrowning21 <dave.browning21@aggiemail.usu.edu>
2017-01-14 18:03:05 -0700

    consolidated to one .js file

--- eventLoop.js
+++ eventLoop.js
@@ -1,6 +1,34 @@
+var eventArray = [];
+
+// shamelessly copied and pasted, then modified from https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+//------------------------------------------------
+var start = null;
+var element = document.getElementById("myCanvas");
+
+function gameLoop(timestamp) {
+ if (!start) start = timestamp;
+   
+ var progress = timestamp - start;
+// console.log('progress: ' + progress);
+ if (progress < 2000) {
+  window.requestAnimationFrame(gameLoop);
+ } 
+ update(timestamp);
+ render();
+}
+//-----------------------------------------------
+
+//window.requestAnimationFrame(gameLoop);
 
 function newEvent(name,interval,rept){
- alert(name + " " + interval + " " + rept);
+ console.log('Event name: ' +name + '; Event interval: ' + interval + '; Event Repetitions: ' + rept);
+ var event = {
+  startTime: performance.now(),
+  event_name: name,
+  event_interval: interval,
+  event_repetition: rept
+ };
+ eventArray.push(event);
 }
 
 function update(time){
@@ -9,12 +37,5 @@ function update(time){
 
 
 function render(){
-
- requestAnimationFrame();
 }
 
-function gameLoop(){
-
- update(timeElapsed);
- render();
-}
