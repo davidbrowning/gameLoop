a8255be1e0de272ed2351c633a20283f4189900f
dmbrowning21 <dave.browning21@aggiemail.usu.edu>
2017-01-20 13:18:50 -0700

    game loop working, just need more logic

--- eventLoop.js
+++ eventLoop.js
@@ -1,41 +1,39 @@
 var eventArray = [];
-
-// shamelessly copied and pasted, then modified from https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-//------------------------------------------------
 var start = null;
-var element = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");
+var killLoop = false;
+
+function setStop(){
+ killLoop = true;
+}
+function setGo(){
+ killLoop = false;
+}
 
 function gameLoop(timestamp) {
  if (!start) start = timestamp;
-   
  var progress = timestamp - start;
-// console.log('progress: ' + progress);
- //if (progress < 2000) {
-  //window.requestAnimationFrame(gameLoop);
- //} 
- update(timestamp);
- render();
+ console.log(eventArray);
+ if(!killLoop){
+  update();
+  render();
+ }
 }
-//-----------------------------------------------
 
 
 function newEvent(name,interval,rept){
  console.log('Event name: ' +name + '; Event interval: ' + interval + '; Event Repetitions: ' + rept);
- var event = {
+ let event = {
   startTime: performance.now(),
   event_name: name,
   event_interval: interval,
   event_repetition: rept
  };
  eventArray.push(event);
- update(performance.now());
- render(eventArray);
- //window.requestAnimationFrame(gameLoop);
+ gameLoop(performance.now());
 }
 
-function update(time){
- console.log(eventArray[0]);
- console.log(eventArray[0].event_name);
+function update(){
 }
 
 
